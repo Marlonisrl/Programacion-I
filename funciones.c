60fe942b0ca5448c595c2cd847c60b71e6c45e35
Marlonisrl <152339443+Marlonisrl@users.noreply.github.com>
2023-11-29 15:32:39 -0500

    funciones.c

--- funciones.c
+++ funciones.c
@@ -3,19 +3,19 @@
 #include "funciones.h"
 
 void mostrarMenu() {
-    printf("\n--- Menú ---\n");
-    printf("1. Ingresar Producto\n");
-    printf("2. Editar Producto\n");
-    printf("3. Eliminar Producto\n");
-    printf("4. Listar Productos\n");
+    printf("\n--- Bienvenidos a Mi Ferreteria ---\n");
+    printf("1. Ingresar Producto Ferretero\n");
+    printf("2. Editar Producto Ferretero\n");
+    printf("3. Eliminar Producto Ferretero\n");
+    printf("4. Lista Productos Ferreteros \n");
     printf("5. Salir\n");
 }
 
-int validarOpcion(int opcion) {
-    return (opcion >= 1 && opcion <= 5);
+int validarOpcion(int escoger) {
+    return (escoger >= 1 && escoger <= 5);
 }
 
-void ingresarProducto(char nombre[][50], int cantidad[], float precio[], float precioTotal[], int *numProductos) {
+void ingresarP(char nombre[][40], int cantidad[], float precio[], float precioTotal[], int *numProductos) {
     printf("\nIngresar Producto\n");
     printf("Nombre: ");
     scanf("%s", nombre[*numProductos]);
@@ -24,15 +24,15 @@ void ingresarProducto(char nombre[][50], int cantidad[], float precio[], float p
     printf("Precio: ");
     scanf("%f", &precio[*numProductos]);
 
-    // Calcular precio total con IVA
+    // Calcular PRECIO total con IVA
     precioTotal[*numProductos] = precio[*numProductos] * (1 + 0.12) * cantidad[*numProductos];
     (*numProductos)++;
 }
 
-void editarProducto(char nombre[][50], int cantidad[], float precio[], float precioTotal[], int numProductos) {
+void editarPr(char nombre[][40], int cantidad[], float precio[], float precioTotal[], int numProductos) {
     printf("\nEditar Producto\n");
     // Mostrar la lista de productos para que el ADMINISTRADOR seleccione cuál editar
-    listarProductos(nombre, cantidad, precio, precioTotal, numProductos);
+    lista(nombre, cantidad, precio, precioTotal, numProductos);
 
     int indice;
     printf("Ingrese el nombre del producto que desea editar: ");
@@ -48,7 +48,7 @@ void editarProducto(char nombre[][50], int cantidad[], float precio[], float pre
         printf("Ingrese el precio unitario:\n ");
         scanf("%f", &precio[indice]);
 
-        // Calcular el nuevo precio total con IVA
+        // Calcular el nuevo PRECIO total con IVA
         precioTotal[indice] = precio[indice] * (1 + 0.12) * cantidad[indice];
 
         printf("Producto editado con éxito.\n");
@@ -57,13 +57,13 @@ void editarProducto(char nombre[][50], int cantidad[], float precio[], float pre
     }
 }
 
-void eliminarProducto(char nombre[][50], int cantidad[], float precio[], float precioTotal[], int *numProductos) {
+void eliminarP(char nombre[][40], int cantidad[], float precio[], float precioTotal[], int *numProductos) {
     printf("\nEliminar Producto\n");
-    listarProductos(nombre, cantidad, precio, precioTotal, *numProductos);
+    lista(nombre, cantidad, precio, precioTotal, *numProductos);
 
-    char nombreEliminar[50];
+    char nombreEliminar[40];
     printf("Ingrese el nombre del producto que desea eliminar: ");
-    scanf("%s", nombreEliminar);
+    scanf("%s", &nombreEliminar);
 
     int cantidadEliminar;
     printf("Ingrese la cantidad que desea eliminar: ");
@@ -74,29 +74,42 @@ void eliminarProducto(char nombre[][50], int cantidad[], float precio[], float p
     int cantidadTotal = 0;  
   // Variable para rastrear la cantidad total del producto
 
-    // Buscar el producto por nombre y cantidad
+    // Buscar el producto por numero de productos
     for (int i = 0; i < *numProductos; i++) {
       //strcmp para comparar
+      //comparación del nombre del producto ingresado con el nombre del producto en la posición i
+        //nombreEliminar
+      // == 0 es igual 
         if (strcmp(nombre[i], nombreEliminar) == 0) {
+          //+= para sumar
             cantidadTotal += cantidad[i];
 
             // Si encontramos suficiente cantidad del producto, establecemos el índice y salimos 
+          //>= 0 es mayor o igual a 0
             if (cantidadTotal >= cantidadEliminar) {
+              //indice = i
                 indice = i;
                 break;
             }
         }
     }
+//!= 0 es diferente
 
     if (indice != -1) {
         // Reducir la cantidad del producto
+      //-= es igual a reducir la cantidad
         cantidad[indice] -= cantidadEliminar;
 
         // Si la cantidad llega a cero, eliminar completamente el producto
         if (cantidad[indice] == 0) {
             // Eliminar el producto moviendo los elementos restantes
           //copia cada cadena de caracteres
+          //i=indice
+          //i
             for (int i = indice; i < *numProductos - 1; i++) {
+              //strcpy copia cadena de caracteres
+              //nombre[i] es el nombre del producto en la posición i
+              //nombre[i+1] es el nombre del producto en la posición i+1
                 strcpy(nombre[i], nombre[i + 1]);
                 cantidad[i] = cantidad[i + 1];
                 precio[i] = precio[i + 1];
@@ -105,14 +118,14 @@ void eliminarProducto(char nombre[][50], int cantidad[], float precio[], float p
             (*numProductos)--;
         }
 
-        printf("Se eliminaron %d unidades del producto '%s' con éxito.\n", cantidadEliminar, nombreEliminar);
+        printf("Se eliminaron %d unidades del producto '%s' exitosamente.\n", cantidadEliminar, nombreEliminar);
     } else {
         printf("No se pudo eliminar el producto '%s' o la cantidad solicitada es mayor a la disponible.\n", nombreEliminar);
     }
 }
 
-void listarProductos(char nombre[][50], int cantidad[], float precio[], float precioTotal[], int numProductos) {
-    printf("\nLista de Productos\n");
+void lista(char nombre[][40], int cantidad[], float precio[], float precioTotal[], int numProductos) {
+    printf("\nLista de Productos Ferreteros\n");
     printf("%-20s %-10s %-15s %-15s\n", "Nombre", "Cantidad", "Precio Unitario", "Precio Total con IVA");
     for (int i = 0; i < numProductos; i++) {
         printf("%-20s %-10d $%-15.2f $%-15.2f\n", nombre[i], cantidad[i], precio[i], precioTotal[i]);
