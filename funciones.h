f176afc70ff9be8de159a2dd1a7069e11bf278d3
Marlonisrl <152339443+Marlonisrl@users.noreply.github.com>
2023-11-28 21:36:22 -0500

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,6 @@
+void mostrarMenu();
+int validarOpcion(int opcion);
+void ingresarProducto(char nombre[][50], int cantidad[], float precio[], float precioTotal[], int *numProductos);
+void editarProducto(char nombre[][50], int cantidad[], float precio[], float precioTotal[], int numProductos);
+void eliminarProducto(char nombre[][50], int cantidad[], float precio[], float precioTotal[], int *numProductos);
+void listarProductos(char nombre[][50], int cantidad[], float precio[], float precioTotal[], int numProductos);
